79d92fb0b7031dbec5b821694b32c6c5381604ba
Stewart Gliniecki <stewgliniecki@gmail.com>
2021-09-01 21:37:32 +0100

    Scoring System

--- rps.js
+++ rps.js
@@ -1,11 +1,8 @@
 let playerSelection = null;
 let buttons = document.querySelectorAll('button');
-let computerScore = document.getElementById('computerScoreText').innerHTML;
-let playerScore = document.getElementById('playerScoreText').innerHTML;
 
-
-
-
+let computerScore = 0 ;
+let playerScore   = 0 ;
 
 
 
@@ -19,16 +16,32 @@ function computerPlay()
 
 function playRound(playerSelection, computerSelection){
 
+    console.log(computerScore,playerScore)
+    document.getElementById('computerScoreText').innerHTML = computerScore;
+document.getElementById('playerScoreText').innerHTML = playerScore;
+    let content = document.getElementById('content');
+
+
+
+
 
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase())
-    {return(`Its a draw ${playerSelection} matches ${computerSelection} `)}
+    {
+        return(content.innerHTML =`Its a draw ${playerSelection} matches ${computerSelection} `)}
 
     else if ((playerSelection.toLowerCase() == "rock"  && computerSelection =="scissors")  ||
     (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper")    ||
     (playerSelection.toLowerCase() == "paper" && computerSelection == "rock"))
-    {return  (`You win against the computer ${playerSelection} beats ${computerSelection}`   ) }
+    {
 
-    else {return (`You lost against a computer.. ${computerSelection} beats ${playerSelection} `)}
+        playerScore++
+        return  (content.innerHTML = `You win against the computer ${playerSelection} beats ${computerSelection}`   ) }
+
+    else {
+
+        computerScore++
+        return (content.innerHTML = `You lost against a computer.. ${computerSelection} beats ${playerSelection} ` )}
+
 
 }
 
